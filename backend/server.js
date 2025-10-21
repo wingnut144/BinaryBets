34502dd3911491e189e0fbfa95399a67d34b7022
wingnut144 <39835369+wingnut144@users.noreply.github.com>
2025-10-21 13:42:31 -0600

    Update server.js
    add bcrypt to import

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import pg from 'pg';
 import fetch from 'node-fetch';
+import bcrypt from 'bcrypt';
 
 const { Pool } = pg;
 const app = express();
