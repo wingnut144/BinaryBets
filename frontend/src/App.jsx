cc34d7a9fa7cf0b8d23bc38e2432dba4db3fc6ab
Steve Grosz <144steve@gmail.com>
2025-11-20 19:41:06 +0000

    Remove non-existent App.css import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import CategoryManagement from './components/CategoryManagement';
 import CreateMarket from './CreateMarket';
 
